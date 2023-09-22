94c46526478fd925a10b609d6879afe9c331b755
yasinkbas <yasin.kbas12@gmail.com>
2023-09-22 16:59:53 +0300

    remove example

--- Example/Pods/Target Support Files/NLab/NLab-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_NLab : NSObject
-@end
-@implementation PodsDummy_NLab
-@end
--- Example/Pods/Target Support Files/Pods-NLab_Tests/Pods-NLab_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NLab_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_NLab_Tests
-@end
